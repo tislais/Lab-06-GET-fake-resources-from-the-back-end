f8735b16cabfae2b3c12a1433fc58aad7b2b4416
Tis Lais <tistististis@gmail.com>
2021-05-03 14:40:48 -0700

    add test setup

---
+++ __tests__/creatures.test.js
@@ -0,0 +1,25 @@
+import app from '../lib/app.js';
+import supertest from 'supertest';
+const request = supertest(app);
+
+// If a GET reqest is made to /api/cats
+// does the server respond with status of 200
+// and body matching the cats data?
+it('GET /api/creatures', async () => {
+  const expected = [
+    {
+      name: 'felix',
+      //...
+    },
+    {
+      name: 'garfield',
+      //...
+    }
+  ];
+
+  const response = await request.get('/api/creatures');
+
+  expect(response.status).toBe(200); // 200 = OK
+  expect(response.body).toEqual(expected);
+
+});
