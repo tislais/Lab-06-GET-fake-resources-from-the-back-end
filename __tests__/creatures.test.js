c4a3f76d09f2cf8bce4891ee5916e41a8a0518a2
Tis Lais <tistististis@gmail.com>
2021-05-03 14:48:29 -0700

    add passing test

--- __tests__/creatures.test.js
+++ __tests__/creatures.test.js
@@ -1,21 +1,13 @@
 import app from '../lib/app.js';
 import supertest from 'supertest';
+import creatureData from '../data/creatures.js';
 const request = supertest(app);
 
 // If a GET reqest is made to /api/cats
 // does the server respond with status of 200
 // and body matching the cats data?
 it('GET /api/creatures', async () => {
-  const expected = [
-    {
-      name: 'felix',
-      //...
-    },
-    {
-      name: 'garfield',
-      //...
-    }
-  ];
+  const expected = creatureData;
 
   const response = await request.get('/api/creatures');
 
