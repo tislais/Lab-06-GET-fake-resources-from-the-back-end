fddccd13626dcbcbb612d74804bdc9e6d5c7b861
Tis Lais <tistististis@gmail.com>
2021-05-03 14:52:31 -0700

    add passing test for array index

--- __tests__/creatures.test.js
+++ __tests__/creatures.test.js
@@ -15,3 +15,20 @@ it('GET /api/creatures', async () => {
   expect(response.body).toEqual(expected);
 
 });
+
+it('GET /api/creatures/1', async () => {
+  const expected = {
+    url:
+      'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+    title: 'UniWhal',
+    description: 'A unicorn and a narwhal nuzzling their horns',
+    keyword: 'narwhal',
+    horns: 1
+  };
+
+  const response = await request.get('/api/creatures/1');
+
+  expect(response.status).toBe(200); // 200 = OK
+  expect(response.body).toEqual(expected);
+
+});
